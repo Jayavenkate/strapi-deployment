29c7a2727638f51a66dca5b93e6be2cbf75c254d
Jayavenkate <jayajaikithuja@gmail.com>
2023-08-25 17:19:34 +0300

    api

---
+++ src/api/footerpage/controllers/footerpage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * footerpage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::footerpage.footerpage');
---
+++ src/api/footerpage/routes/footerpage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * footerpage router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::footerpage.footerpage');
